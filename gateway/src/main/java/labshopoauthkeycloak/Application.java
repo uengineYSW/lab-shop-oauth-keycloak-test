64084609896c3178aed5f6ce93414c6ba252f5d3
uengineYSW <112850933+uengineYSW@users.noreply.github.com>
2023-07-19 13:05:44 +0900

    Update and rename Application.java to Application.java

--- gateway/src/main/java/labshopoauthkeycloak/Application.java
+++ gateway/src/main/java/labshopoauthkeycloak/Application.java
@@ -1,4 +1,4 @@
-package labshopoauth-keycloak;
+package labshopoauthkeycloak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
