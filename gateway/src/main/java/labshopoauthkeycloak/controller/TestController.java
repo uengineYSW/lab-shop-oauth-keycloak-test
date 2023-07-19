223e1680b25485b19465b83421f7529dc07779ae
uengineYSW <112850933+uengineYSW@users.noreply.github.com>
2023-07-19 13:07:31 +0900

    Update and rename TestController.java to TestController.java

--- gateway/src/main/java/labshopoauthkeycloak/controller/TestController.java
+++ gateway/src/main/java/labshopoauthkeycloak/controller/TestController.java
@@ -1,4 +1,4 @@
-package labshopoauth-keycloak.controller;
+package labshopoauthkeycloak.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
